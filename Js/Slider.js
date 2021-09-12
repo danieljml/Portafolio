a8b60d06e076a81a744aed920253e9d766c44ecb
danieljml <danielgarrido425@gmail.com>
2021-09-12 15:16:58 -0400

    remove window load of slider

--- Js/Slider.js
+++ Js/Slider.js
@@ -1,19 +1,16 @@
-function load() {
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".bxs-caret-right-circle",
-      prevEl: ".bxs-caret-left-circle",
-    },
-  });
-}
-window.onload = load;
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".bxs-caret-right-circle",
+    prevEl: ".bxs-caret-left-circle",
+  },
+});
