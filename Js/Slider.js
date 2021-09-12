a8a0aa5db594c964cbea729c0f241d457190e0d9
danieljml <danielgarrido425@gmail.com>
2021-09-12 08:34:53 -0400

    window load slider

--- Js/Slider.js
+++ Js/Slider.js
@@ -1,16 +1,19 @@
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 4500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".bxs-caret-right-circle",
-    prevEl: ".bxs-caret-left-circle",
-  },
-});
+function load() {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".bxs-caret-right-circle",
+      prevEl: ".bxs-caret-left-circle",
+    },
+  });
+}
+window.onload = load;
