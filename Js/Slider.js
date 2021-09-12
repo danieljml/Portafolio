baf61c160225eaefd476ed24a3f6fc51dcb7550e
danieljml <danielgarrido425@gmail.com>
2021-09-12 15:42:29 -0400

    slider 2

--- Js/Slider.js
+++ Js/Slider.js
@@ -14,3 +14,20 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".bxs-caret-left-circle",
   },
 });
+
+var swiper = new Swiper(".mySwiper-1", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
